cac383b2a5f41d01104b19c06731584779fd326d
Dhawal Verma <dhawalverma@icloud.com>
2018-09-11 14:47:56 -0500

    Initial setup

--- controllers/live.js
+++ controllers/live.js
@@ -14,8 +14,8 @@ class LiveController extends Telegram.TelegramBaseCallbackQueryController{
             'Premier League': () => {}, //will be on first line
             'La Liga': () => {}, //will be on second line
             'Champions League': () => {
-                $.sendMessage('--- The Champions ---\n');
-                $.sendPhoto({ url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/UEFA_Champions_League_logo_2.svg', filename: 'UCL.jpg'})
+                //$.sendMessage('--- The Champions ---\n');
+                //$.sendPhoto({ url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/UEFA_Champions_League_logo_2.svg', filename: 'UCL.jpg'})
             }
         });
     }
